d5b53b8eb373b91d92d532b861b88ad65f545685
VVheyn <vanlimustafa0@gmail.com>
2021-12-02 12:11:59 +0300

    Add project files.

---
+++ SignalRExample_Project/Controllers/ChatController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRExample_Project.Hubs;
+using System.Threading.Tasks;
+
+namespace SignalRExample_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        ChatHub _hub;
+        public ChatController(ChatHub hub)
+        {
+            _hub = hub;
+        }
+
+        [HttpPost("Notification")]
+        public async Task<IActionResult> PostNotification(string message)
+        {
+            await _hub.Clients.All.SendAsync("Notification",message);
+            return Ok();
+        }
+    }
+}
---
+++ SignalRExample_Project/Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SignalRExample_Project.Hubs
+{
+    public class ChatHub:Hub
+    {
+        static HashSet<string> users = new HashSet<string>();
+        public async override Task OnConnectedAsync()
+        {
+            users.Add(Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+        public async Task SendMessageAsync(string message)
+        {
+            if (users.Contains(Context.ConnectionId))
+            {
+                await Clients.All.SendAsync("receiveMessage", Context.ConnectionId + ":" + message);
+            }
+        }
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            users.Remove(Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
